398de809c03b41867dcb592ea4d80e362f1a391f
Orangeninja1 <orangegary1@gmail.com>
2022-02-19 22:57:56 -0500

    Added additional servers to ping as well as changed the directory that results are stored in.

--- Packet Loss.ps1
+++ Packet Loss.ps1
@@ -4,10 +4,22 @@ $Ping = ping -n 10 8.8.8.8
 $Loss = [regex]::match($Ping, $regex).Groups[1]
 $Time = Get-Date
 $Results = "$time 8.8.8.8 $Loss"
-$Results | Out-File -Append -Encoding UTF8 8.8.8.8.txt
+$Results | Out-File -Append -Encoding UTF8 C:\PacketLoss\8.8.8.8.txt
 
 $Ping = ping -n 10 8.8.4.4
 $Loss = [regex]::match($Ping, $regex).Groups[1]
 $Time = Get-Date
 $Results = "$time 8.8.4.4 $Loss"
-$Results | Out-File -Append -Encoding UTF8 8.8.4.4.txt
+$Results | Out-File -Append -Encoding UTF8 C:\PacketLoss\8.8.4.4.txt
+
+$Ping = ping -n 10 1.1.1.1
+$Loss = [regex]::match($Ping, $regex).Groups[1]
+$Time = Get-Date
+$Results = "$time 1.1.1.1 $Loss"
+$Results | Out-File -Append -Encoding UTF8 C:\PacketLoss\1.1.1.1.txt
+
+$Ping = ping -n 10 pool.ntp.org
+$Loss = [regex]::match($Ping, $regex).Groups[1]
+$Time = Get-Date
+$Results = "$time pool.ntp.org $Loss"
+$Results | Out-File -Append -Encoding UTF8 C:\PacketLoss\pool.ntp.org.txt

125a4c2188d9f559eec00a2d93e786066ba891cb
Orangeninja1 <orangegary1@gmail.com>
2022-02-19 23:07:19 -0500

    Changed it to ping 100 times per IP per run.

--- Packet Loss.ps1
+++ Packet Loss.ps1
@@ -1,24 +1,24 @@
 $regex = [regex]"\((.*)\)" 
 
-$Ping = ping -n 10 8.8.8.8
+$Ping = ping -n 100 8.8.8.8
 $Loss = [regex]::match($Ping, $regex).Groups[1]
 $Time = Get-Date
 $Results = "$time 8.8.8.8 $Loss"
 $Results | Out-File -Append -Encoding UTF8 C:\PacketLoss\8.8.8.8.txt
 
-$Ping = ping -n 10 8.8.4.4
+$Ping = ping -n 100 8.8.4.4
 $Loss = [regex]::match($Ping, $regex).Groups[1]
 $Time = Get-Date
 $Results = "$time 8.8.4.4 $Loss"
 $Results | Out-File -Append -Encoding UTF8 C:\PacketLoss\8.8.4.4.txt
 
-$Ping = ping -n 10 1.1.1.1
+$Ping = ping -n 100 1.1.1.1
 $Loss = [regex]::match($Ping, $regex).Groups[1]
 $Time = Get-Date
 $Results = "$time 1.1.1.1 $Loss"
 $Results | Out-File -Append -Encoding UTF8 C:\PacketLoss\1.1.1.1.txt
 
-$Ping = ping -n 10 pool.ntp.org
+$Ping = ping -n 100 pool.ntp.org
 $Loss = [regex]::match($Ping, $regex).Groups[1]
 $Time = Get-Date
 $Results = "$time pool.ntp.org $Loss"

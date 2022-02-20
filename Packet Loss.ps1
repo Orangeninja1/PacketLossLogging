541dffb3fbea0b512673ac3bda0577134dcdcd16
Orangeninja1 <orangegary1@gmail.com>
2022-02-20 11:27:52 -0500

    Converted to using functions instead of copying the code block. Added OutputDirectory variable.

--- Packet Loss.ps1
+++ Packet Loss.ps1
@@ -1,25 +1,17 @@
+$OutputDirectory = "C:\PacketLoss\"
 $regex = [regex]"\((.*)\)" 
 
-$Ping = ping -n 100 8.8.8.8
-$Loss = [regex]::match($Ping, $regex).Groups[1]
-$Time = Get-Date
-$Results = "$time 8.8.8.8 $Loss"
-$Results | Out-File -Append -Encoding UTF8 C:\PacketLoss\8.8.8.8.txt
+function PacketLossTest
+{
+	param($Destination)
+	$Ping = ping -n 100 "$Destination"
+	$Loss = [regex]::match($Ping, $regex).Groups[1]
+	$Time = Get-Date
+	$Results = "$time $Destination $Loss"
+	$Results | Out-File -Append -Encoding UTF8 $OutputDirectory$Destination.txt
+}
 
-$Ping = ping -n 100 8.8.4.4
-$Loss = [regex]::match($Ping, $regex).Groups[1]
-$Time = Get-Date
-$Results = "$time 8.8.4.4 $Loss"
-$Results | Out-File -Append -Encoding UTF8 C:\PacketLoss\8.8.4.4.txt
-
-$Ping = ping -n 100 1.1.1.1
-$Loss = [regex]::match($Ping, $regex).Groups[1]
-$Time = Get-Date
-$Results = "$time 1.1.1.1 $Loss"
-$Results | Out-File -Append -Encoding UTF8 C:\PacketLoss\1.1.1.1.txt
-
-$Ping = ping -n 100 pool.ntp.org
-$Loss = [regex]::match($Ping, $regex).Groups[1]
-$Time = Get-Date
-$Results = "$time pool.ntp.org $Loss"
-$Results | Out-File -Append -Encoding UTF8 C:\PacketLoss\pool.ntp.org.txt
+PacketLossTest -Destination "8.8.8.8"
+PacketLossTest -Destination "8.8.4.4"
+PacketLossTest -Destination "1.1.1.1"
+PacketLossTest -Destination "wikipedia.org"

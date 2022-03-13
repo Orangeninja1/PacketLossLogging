c518f7782ba05eb22b692643d743ff54909c63aa
Orangeninja1 <orangegary1@gmail.com>
2022-03-12 19:08:06 -0500

    Added $NumberOfPings variable and added function for exporting to CSV and to CSV and TXT at the same time

--- Packet Loss.ps1
+++ Packet Loss.ps1
@@ -1,17 +1,48 @@
 $OutputDirectory = "C:\PacketLoss\"
+$NumberOfPings = 100
 $regex = [regex]"\((.*)\)" 
 
 function PacketLossTest
 {
 	param($Destination)
-	$Ping = ping -n 100 "$Destination"
+	$Ping = ping -n $NumberOfPings "$Destination"
 	$Loss = [regex]::match($Ping, $regex).Groups[1]
 	$Time = Get-Date
 	$Results = "$time $Destination $Loss"
 	$Results | Out-File -Append -Encoding UTF8 $OutputDirectory$Destination.txt
 }
 
-PacketLossTest -Destination "8.8.8.8"
-PacketLossTest -Destination "8.8.4.4"
-PacketLossTest -Destination "1.1.1.1"
-PacketLossTest -Destination "wikipedia.org"
+function PacketLossTestCSV
+{
+	param($Destination)
+	$Ping = ping -n $NumberOfPings "$Destination"
+	$Loss = [regex]::match($Ping, $regex).Groups[1]
+	$Time = Get-Date
+	$CSVResults = [ordered]@{ 
+            'Time'= $Time;
+            'Destination'= $Destination;
+            'Packet Loss' = $Loss}
+	$CSVOutput = New-Object -TypeName PSObject -Property $CSVResults
+	$CSVOutput | Export-Csv -path $OutputDirectory$Destination.csv -append
+}
+
+function PacketLossTestTXTandCSV
+{
+	param($Destination)
+	$Ping = ping -n $NumberOfPings "$Destination"
+	$Loss = [regex]::match($Ping, $regex).Groups[1]
+	$Time = Get-Date
+	$Results = "$time $Destination $Loss"
+	$Results | Out-File -Append -Encoding UTF8 $OutputDirectory$Destination.txt
+	$CSVResults = [ordered]@{ 
+            'Time'= $Time;
+            'Destination'= $Destination;
+            'Packet Loss' = $Loss}
+	$CSVOutput = New-Object -TypeName PSObject -Property $CSVResults
+	$CSVOutput | Export-Csv -path $OutputDirectory$Destination.csv -append
+}
+
+PacketLossTestTXTandCSV -Destination "8.8.8.8"
+PacketLossTestTXTandCSV -Destination "8.8.4.4"
+PacketLossTestTXTandCSV -Destination "1.1.1.1"
+PacketLossTestTXTandCSV -Destination "wikipedia.org"
